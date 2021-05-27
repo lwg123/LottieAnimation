1d2cb343b64c1eb0ef80f62710f6da7d4a38161d
lwg80291911 <weiguang0925@cmbchina.com>
2021-05-27 16:14:59 +0800

    add project

---
+++ LottieAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieAnimation
+//
+//  Created by weiguang on 2021/5/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
